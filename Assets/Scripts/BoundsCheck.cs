f813bfa71196b022eef5bbe9f65d7c91af204fe1
peterduong968 <54965701+peterduong968@users.noreply.github.com>
2019-10-24 21:38:42 -0400

    Step 3. Added Bounds

---
+++ Assets/Scripts/BoundsCheck.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundsCheck : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    public float radius = 1f;
+
+    [Header("Set Dynamically")]
+    public float camWidth;
+    public float camHeight;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        camHeight = Camera.main.orthographicSize;
+        camWidth = camHeight * Camera.main.aspect;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 pos = transform.position;
+        if( pos.x > camWidth - radius)
+        {
+            pos.x = camWidth - radius;
+        }
+
+        if(pos.x < -camWidth + radius)
+        {
+            pos.x = -camWidth + radius;
+        }
+
+        if(pos.y > camHeight - radius)
+        {
+            pos.y = camHeight - radius;
+        }
+
+        if(pos.y < -camHeight + radius)
+        {
+            pos.y = -camHeight + radius;
+        }
+
+        transform.position = pos;
+    }
+
+    private void OnDrawGizmos()
+    {
+        if(!Application.isPlaying) return;
+        Vector3 boundSize = new Vector3(camWidth * 2, camHeight * 2, .1f);
+        Gizmos.DrawWireCube(Vector3.zero, boundSize);
+    }
+}
